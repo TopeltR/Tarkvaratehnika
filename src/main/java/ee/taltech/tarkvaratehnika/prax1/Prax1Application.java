70e1f8fb1e416432c58ed6acb74a0869b3a2e6b0
r.ryngenen@gmail.com <Maarika5>
2019-02-07 09:49:48 +0200

    First commit

---
+++ src/main/java/ee/taltech/tarkvaratehnika/prax1/Prax1Application.java
@@ -0,0 +1,14 @@
+package ee.taltech.tarkvaratehnika.prax1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prax1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prax1Application.class, args);
+    }
+
+}
+
---
+++ src/test/java/ee/taltech/tarkvaratehnika/prax1/Prax1ApplicationTests.java
@@ -0,0 +1,17 @@
+package ee.taltech.tarkvaratehnika.prax1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Prax1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
